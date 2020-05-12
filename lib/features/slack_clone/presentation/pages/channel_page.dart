d5e947556e6b27f85fb1a58d8a7c9f797b03a33d
sky <solankish66@gmail.com>
2020-05-12 18:18:58 +0530

    ChannelPage and Conversation Page Seperated

---
+++ lib/features/slack_clone/presentation/pages/channel_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class ChannelPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blueAccent,
+      width: double.infinity,
+      child: Text("Channels"),
+    );
+  }
+}
--- lib/features/slack_clone/presentation/pages/home_page.dart
+++ lib/features/slack_clone/presentation/pages/home_page.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:slackclone/features/slack_clone/presentation/pages/channel_page.dart';
+import 'package:slackclone/features/slack_clone/presentation/pages/conversation_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: Container(
-            child: Text("Home Page"),
-          ),
-        ),
-      ),
-    );
+        child: PageView(
+      children: [
+        ChannelPage(),
+        ConversationPage()
+      ],
+    ));
   }
 }
