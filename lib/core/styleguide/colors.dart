870667068dd61428dc1b1a9a62c13ab26978c3e9
Aakash Solanki <solankish66@gmail.com>
2020-05-13 10:11:40 +0530

    Assets Added, colors and text_styles added

---
+++ lib/core/styleguide/colors.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+
+// Color palette for the un themed pages
+class KColors {
+  static Color primaryColor = Color(0xFF393A3E);
+  static Color secondaryColor = Color(0xFF4586EA);
+  static Color primaryTextColor = Color(0xFFFFFFFF);
+  static Color secondaryTextColor = Color(0xFFFFFFFF);
+
+  //////////////////****Channel Page****BEGIN*****//////////////////
+
+
+  //************Background*************//
+  static Color channelPageBackgroundColor = Color(0xFF202125);
+
+  //************Search TextField*************//
+  static Color channelSearchTextFieldContainerColor = Color(0xFF393A3E);
+  static Color channelSearchTextFieldTextColor = Color(0xFF808080);
+  static Color channelSearchTextFieldSearchIconColor = Color(0xFF808080);
+
+  //************Channels Title*************//
+  static Color channelsTitleTextColor = Color(0xFFFFFFFF);
+
+  //************Channel List*************//
+  static Color channelListTextColor = const Color(0xFF9AA1A9);
+
+
+  //////////////////****Channel Page****END*****//////////////////
+
+  //////////////////****Bottom Bar****BEGIN*****//////////////////
+
+
+  static Color bottomBarBackground = const Color(0xFF16181C);
+  static Color bottomBarIconTextColor = const Color(0xFF9AA1A9) ;
+  static Color bottomBarSelectedIconTextColor = const Color(0xFFCFD3D6);
+
+
+  //////////////////****Bottom Bar****END*****//////////////////
+
+  //////////////////****Conversation Page****BEGIN*****//////////////////
+
+
+  //************Background*************//
+  static Color conversationPageBackgroundColor = Color(0xFF202125);
+
+  //************Appbar*************//
+  static Color appBarBackgroundColor = Color(0xFF393A3E);
+  static Color appBarTextIconColor = const Color(0xFFFFFFFF);
+
+  //************Post*************//
+  static Color postTextColor = Color(0xFFFFFFFF);
+  static Color postHashTagTextColor = Color(0xFF4586EA);
+  static Color postLinkTextColor = Color(0xFF3D8BA7);
+  static Color postCardColor = Color(0xFF1A1D22);
+  static Color postTimeStampTextColor = Color(0xFFFFFFFF).withOpacity(0.5);
+
+  //************Post TextField*************//
+  static Color postTextFieldContainerColor = Color(0xFF393A3E);
+  static Color postTextFieldTextColor = Color(0xFFFFFFFF);
+  static Color kcTextFieldHintTextColor = Color(0xFF808080);
+  static Color postTextFieldIconColor = Color(0xFF808080);
+
+
+  //////////////////Conversation Page****END*****//////////////////
+
+}
---
+++ lib/core/styleguide/text_styles.dart
@@ -0,0 +1,98 @@
+import 'package:flutter/material.dart';
+import 'colors.dart';
+
+
+//TextField
+class KTextField {
+
+  //////////////////****Channel Page****BEGIN*****//////////////////
+
+
+  //************Search TextField Title*************//
+  static TextStyle channelSearchTextFieldTextStyle = TextStyle(
+    fontSize: 18.0,
+    color: KColors.channelSearchTextFieldTextColor,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Roboto',
+  );
+
+//************Channels Title*************//
+  static TextStyle channelsTitleTextStyle = TextStyle(
+    fontSize: 18.0,
+    color: KColors.channelsTitleTextColor,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle channelAddButtonTextStyle = TextStyle(
+    fontSize: 16.0,
+    color: KColors.channelsTitleTextColor,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Roboto',
+  );
+
+//************Channel List*************//
+  static TextStyle defaultChannelItemTextStyle = TextStyle(
+    fontSize: 16.0,
+    color: KColors.channelListTextColor,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Roboto',
+  );
+
+
+//////////////////****Channel Page****END*****//////////////////
+
+//////////////////****Conversation Page****BEGIN*****//////////////////
+
+
+//************AppBar*************//
+  static TextStyle appBarTextStyle = TextStyle(
+    fontSize: 24.0,
+    color: KColors.appBarTextIconColor,
+    fontWeight: FontWeight.w500,
+    fontFamily: 'Roboto',
+  );
+
+//************Post*************//
+  static TextStyle postTextStyle = TextStyle(
+    fontSize: 16.0,
+    color: KColors.postTextColor,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle postTimeStampTextColor = TextStyle(
+    fontSize: 10.0,
+    color: KColors.postTimeStampTextColor,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle postHashTagTextStyle = TextStyle(
+    fontSize: 16.0,
+    color: KColors.postHashTagTextColor,
+    fontWeight: FontWeight.w600,
+    fontFamily: 'Roboto',
+  );
+
+  static TextStyle postHashLinkTextStyle = TextStyle(
+    fontSize: 16.0,
+    color: KColors.postLinkTextColor,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Roboto',
+  );
+
+
+//************PostTextField*************//
+  static TextStyle postTextFieldTextStyle = TextStyle(
+    fontSize: 18.0,
+    color: KColors.postTextFieldTextColor,
+    fontWeight: FontWeight.normal,
+    fontFamily: 'Roboto',
+  );
+
+
+//////////////////****Conversation Page****END*****//////////////////
+
+}
+
